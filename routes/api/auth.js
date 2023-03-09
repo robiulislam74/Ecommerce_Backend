75eeaf3ce933189e5e2b227e6674374c53107a6b
Robiul Islam Sumon <114084192+SumonAhmed74@users.noreply.github.com>
2023-03-09 08:19:05 +0600

    Err check

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,19 @@ const express = require("express")
 const _ = express.Router()
 
 _.get('/registration',(req,res)=>{
-    res.send(req.body)
+     const {fullName,email,phone}=req.body
+
+     if(!fullName){
+        return res.send("Err: Please enter your FullName!")
+     }
+     if(!email){
+        return res.send("Err: Please enter your email!")
+     }
+     if(!phone){
+        return res.send("Err: Please enter your phoneNumber!")
+     }
+
+     res.send(req.body)
 })
 
 module.exports= _;
